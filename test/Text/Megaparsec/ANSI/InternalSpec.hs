4f3d59e2e4987b1ae3dc2489d348431989f03864
Rehno Lindeque <errantkid@gmail.com>
2018-09-03 16:24:56 -0400

    test: psingleton

--- test/Text/Megaparsec/ANSI/InternalSpec.hs
+++ test/Text/Megaparsec/ANSI/InternalSpec.hs
@@ -2,9 +2,13 @@ module Text.Megaparsec.ANSI.InternalSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.Megaparsec
+import Test.AdHoc
 
 import Text.Megaparsec
 import Text.Megaparsec.ANSI.Internal
 
 spec :: Spec
-spec = pure ()
+spec =
+  describe "psingleton" $ do
+    it "works" $ do
+      parse (psingleton anySingle :: Parser String) "" "a" `shouldParse` "a"
