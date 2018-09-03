35c3a01ea81c5a53c714572f966c9a094b804702
Rehno Lindeque <errantkid@gmail.com>
2018-09-03 16:27:41 -0400

    Add pappend helper

--- src/Text/Megaparsec/ANSI/Internal.hs
+++ src/Text/Megaparsec/ANSI/Internal.hs
@@ -1,16 +1,22 @@
 {-# language OverloadedStrings, FlexibleContexts, ScopedTypeVariables, TypeFamilies #-}
 
--- | Internal helpers for building terminal parsers
+-- | Internal helpers that normally do not need to be imported.
 module Text.Megaparsec.ANSI.Internal
   ( psingleton
+  , pappend
   ) where
 
 import Text.Megaparsec
 import Data.Proxy (Proxy (..))
-
-
+import Data.Semigroup (Semigroup, (<>))
+import Control.Applicative (liftA2)
 
 -- | Turn a character parser into a string parser
 psingleton :: forall e s m. (Stream s, MonadParsec e s m) => m (Token s) -> m (Tokens s)
 psingleton = fmap (tokenToChunk (Proxy :: Proxy s))
 {-# INLINE psingleton #-}
+
+-- | Append two parse results without requiring @Semigroup (m a)@
+pappend :: (MonadParsec e s m, Semigroup a) => m a -> m a -> m a
+pappend = liftA2 (<>)
+{-# INLINE pappend #-}
--- test/Text/Megaparsec/ANSI/InternalSpec.hs
+++ test/Text/Megaparsec/ANSI/InternalSpec.hs
@@ -8,7 +8,11 @@ import Text.Megaparsec
 import Text.Megaparsec.ANSI.Internal
 
 spec :: Spec
-spec =
+spec = do
   describe "psingleton" $ do
     it "works" $ do
       parse (psingleton anySingle :: Parser String) "" "a" `shouldParse` "a"
+
+  describe "pappend" $ do
+    it "works" $ do
+      parse (chunk "abc" `pappend` chunk "de" :: Parser String) "" "abcdefgh" `shouldParse` "abcde"
