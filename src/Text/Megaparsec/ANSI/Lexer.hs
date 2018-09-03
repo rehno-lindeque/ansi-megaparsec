8166e2a906ebc699066477254fb9638f06e76b54
Rehno Lindeque <errantkid@gmail.com>
2018-09-03 17:39:40 -0400

    Lexer for ANSI control sequences (prefixed by CSI)

--- src/Text/Megaparsec/ANSI/Lexer.hs
+++ src/Text/Megaparsec/ANSI/Lexer.hs
@@ -10,15 +10,20 @@ module Text.Megaparsec.ANSI.Lexer
     -- * C1 escape sequence variations
 
     -- $escvar
+  , escC1
   , escCsi
 
     -- * C1 single 8-bit character variations
+
     -- $singlevar
+  , single8BitC1
   , single8BitCsi
+
     -- * C1 single 8-bit character / escape sequence compatible parsers
 
     -- $c1compat
   , anyCsi
+  , anyAnsiControlSequence
 
     -- * Trivial parsers
 
@@ -29,6 +34,7 @@ module Text.Megaparsec.ANSI.Lexer
 
 import Text.Megaparsec
 import Text.Megaparsec.ANSI.Internal
+import Text.Megaparsec.ANSI.Common
 import Data.Semigroup (Semigroup, (<>))
 import Data.Proxy
 
@@ -60,6 +66,11 @@ escCsi =
     toChunk c = tokenToChunk (Proxy :: Proxy s) (toEnum c)
 {-# INLINE escCsi #-}
 
+-- | Escape sequence equivalents to the C1 character set
+escC1 :: forall e s m. (MonadParsec e s m, Enum (Token s), Semigroup (Tokens s)) => m (Tokens s)
+escC1 = (psingleton esc `pappend` psingleton (satisfy (\c -> c >= toEnum 0x40 && c <= toEnum 0x5F))) <?> "ESC_C1"
+{-# INLINE escC1 #-}
+
 -- $singlevar
 -- Note that the ECMA-48 standard allows for certain ESC-prefixed control sequences to be substituted by a single 8-bit
 -- C1 control code character (in 8-bit environments). However, according to
@@ -69,6 +80,12 @@ escCsi =
 -- For that reason it is recommended to use ESC-prefixed codes. However, in some rare instances where this is not possible
 -- the compatible parsers in section following this one can be used. Individual c1 character parsers are provided here for
 -- the sake of completeness only.
+
+-- | Single character, C1 character set
+single8BitC1 :: (MonadParsec e s m, Ord (Token s), Enum (Token s)) => m (Token s)
+single8BitC1 = satisfy (\c -> c >= toEnum 0x80 && c <= toEnum 0x9F) <?> "C1"
+{-# INLINE single8BitC1 #-}
+
 -- | Single character, control sequence introducer
 single8BitCsi :: (MonadParsec e s m, Enum (Token s)) => m (Token s)
 single8BitCsi = single (toEnum 0x9b)
@@ -83,6 +100,15 @@ anyCsi ExcludeSingle8BitC1 = escCsi <?> "CSI"
 anyCsi IncludeSingle8BitC1 = (escCsi <|> psingleton single8BitCsi) <?> "CSI"
 {-# INLINE anyCsi #-}
 
+
+-- | Any CSI-prefixed control sequence.
+anyAnsiControlSequence :: (MonadParsec e s m, Enum (Token s), Semigroup (Tokens s)) => Single8BitC1Compatibility -> m (Tokens s)
+anyAnsiControlSequence s8c1Compat = anyCsi s8c1Compat `pappend` parameterBytes `pappend` intermediateBytes `pappend` finalByte
+  where
+    parameterBytes = takeWhileP (Just "CSI parameter byte") isCsParameterByte
+    intermediateBytes = takeWhileP (Just "CSI intermediate byte") isCsIntermediateByte
+    finalByte = psingleton (satisfy isCsFinalByte)
+{-# INLINE anyAnsiControlSequence #-}
 -- $trivial
 
 -- | Escape character, commonly prefixed to escape sequences.
--- test/Text/Megaparsec/ANSI/LexerSpec.hs
+++ test/Text/Megaparsec/ANSI/LexerSpec.hs
@@ -17,6 +17,18 @@ spec = do
       it "fails" $ do
         parse (esc :: Parser Char) "" `shouldFailOn` "a"
 
+  describe "escC1" $ do
+    context "when given a C1 ESC code"  $
+      it "works" $ do
+        parse (escC1 :: Parser String) "" "\ESC@" `shouldParse` "\ESC@"
+        parse (escC1 :: Parser String) "" "\ESC_" `shouldParse` "\ESC_"
+
+  describe "single8BitC1" $ do
+    context "when given a single 8-bit C1" $
+      it "works" $ do
+        parse (single8BitC1 :: Parser Char) "" "\x80" `shouldParse` '\x80'
+        parse (single8BitC1 :: Parser Char) "" "\x9f" `shouldParse` '\x9f'
+
   describe "anyCsi" $ do
     context "when given ESC+CSI" $
       it "works" $ do
@@ -25,3 +37,22 @@ spec = do
     context "when given a single 8-bit CSI" $
       it "works" $ do
         parse (anyCsi IncludeSingle8BitC1 :: Parser String) "" "\x9b" `shouldParse` "\x9b"
+
+  describe "anyAnsiControlSequence" $ do
+    context "when given cursor positions" $
+      it "works" $ do
+        parse (anyAnsiControlSequence ExcludeSingle8BitC1 :: Parser String) "" "\ESC[20;20H" `shouldParse` "\ESC[20;20H"
+        parse (anyAnsiControlSequence ExcludeSingle8BitC1 :: Parser String) "" "\ESC[20;20f" `shouldParse` "\ESC[20;20f"
+    context "when given a 4-bit SGR color" $
+      it "works" $ do
+        parse (anyAnsiControlSequence ExcludeSingle8BitC1 :: Parser String) "" "\ESC[31m" `shouldParse` "\ESC[31m"
+        parse (anyAnsiControlSequence ExcludeSingle8BitC1 :: Parser String) "" "\ESC[1;31m" `shouldParse` "\ESC[1;31m"
+        parse (anyAnsiControlSequence ExcludeSingle8BitC1 :: Parser String) "" "\ESC[39;49m" `shouldParse` "\ESC[39;49m"
+
+    context "when given a 8-bit SGR color" $
+      it "works" $ do
+        parse (anyAnsiControlSequence ExcludeSingle8BitC1 :: Parser String) "" "\ESC[38;5;206;48;5;57m" `shouldParse` "\ESC[38;5;206;48;5;57m"
+
+    context "when given a 24-bit SGR color" $
+      it "works" $ do
+        parse (anyAnsiControlSequence ExcludeSingle8BitC1 :: Parser String) "" "\ESC[38;2;255;82;197;48;2;155;106;0m" `shouldParse` "\ESC[38;2;255;82;197;48;2;155;106;0m"
